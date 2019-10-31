a2a182d55cc7c6ee3dcf8758424beb941a01c123
matthieuhodin <42959826+matthieuhodin@users.noreply.github.com>
2019-10-31 11:32:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+function actualiserTotalColonne(columnId){
+   const resultCards = await github.projects.listCards({column_id: columnId });
+  
+  console.log('resultCards :', resultCards);
+  
+}
+
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
@@ -10,6 +17,12 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+  
+  actualiserTotalColonne( github.context.payload.project_card.column_id);
+  if( github.context.payload.changes && github.context.payload.changes.column_id && github.context.payload.changes.column_id.from){
+    actualiserTotalColonne(github.context.payload.changes.column_id.from);
+  }
+  
 } catch (error) {
   core.setFailed(error.message);
 }
