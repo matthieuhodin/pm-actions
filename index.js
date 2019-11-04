010b1d60532a07c39488d9bcbb67b5a3c8539fb5
matthieuhodin <42959826+matthieuhodin@users.noreply.github.com>
2019-11-04 14:32:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,32 +5,44 @@ const core = require('@actions/core');
 async function actualiserTotalColonne(octokit, columnId){
    console.log('actualiserTotalColonne:'+columnId);
    var total=0;
+   var cardStat=null;
    const resultCards = await octokit.projects.listCards({column_id: columnId });
   
    for(var iCard=0; iCard<  resultCards.data.length; iCard++){
       var card=resultCards.data[iCard];
-      var segments= card.content_url.split('/');
-      
-      var owner = segments[segments.length-4];
-      var repo=segments[segments.length-3];
-      var issue_number = segments[segments.length-1];
-      const detailIssue = await octokit.issues.get({
-                             owner,
-                             repo,
-                             issue_number
-                           });
-      console.log('DETAIL Issue '+issue_number);
-      console.log(detailIssue);
-      
-      for(var iLabel =0 ; iLabel < detailIssue.data.labels.length; iLabel++){
-         var label = detailIssue.data.labels[iLabel];
-         
-         if( label.name.indexOf('size:')==0){
-            total+= parseFloat(label.name.replace('size:',''))
+      if( (card.content_url||'').indexOf('/issues/')>0){
+         var segments= card.content_url.split('/');
+
+         var owner = segments[segments.length-4];
+         var repo=segments[segments.length-3];
+         var issue_number = segments[segments.length-1];
+         const detailIssue = await octokit.issues.get({
+                                owner,
+                                repo,
+                                issue_number
+                              });
+
+         for(var iLabel =0 ; iLabel < detailIssue.data.labels.length; iLabel++){
+            var label = detailIssue.data.labels[iLabel];
+
+            if( label.name.indexOf('size:')==0){
+               total+= parseFloat(label.name.replace('size:',''))
+            }
          }
       }
       
+      if( (card.note||'').indexOf('<!--STAT-->')==0){
+         cardStat= card;
+      }
+      
    }
+   if (cardStat==null){
+      const createCardResult= await octokit.projects.createCard({column_id: columnId , note:'<!--STAT-->'})
+      cardStat= createCardResult.data;
+   }
+   
+   const updateCard= await  octokit.projects.updateCard({card_id:cardStat.card_id, note:'<!--STAT-->Total : '+ total})
+   
    console.log('Total :'+total);
     //     "content_url": "https://api.github.com/repos/matthieuhodin/pm-actions/issues/1"
    
