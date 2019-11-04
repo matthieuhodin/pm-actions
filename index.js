eba64c88cab98e07a2bd7c2491bbe317531e41c2
matthieuhodin <42959826+matthieuhodin@users.noreply.github.com>
2019-11-04 11:42:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,48 @@
-const core = require('@actions/core');
 const github = require('@actions/github');
+const core = require('@actions/core');
+
 
 async function actualiserTotalColonne(octokit, columnId){
-   
    console.log('actualiserTotalColonne:'+columnId);
    const resultCards = await octokit.projects.listCards({column_id: columnId });
   
    console.log('resultCards :');
-   console.log(JSON.stringify(resultCards, undefined, 2));
-  
+   console.log(JSON.stringify(resultCards, undefined, 2));  
 }
 
-try {
+
+async function run() {
+    // This should be a token with access to your repository scoped in as a secret.
+    // The YML workflow will need to set myToken with the GitHub Secret Token
+    // myToken: ${{ secrets.GITHUB_TOKEN }}
+    // https://help.github.com/en/articles/virtual-environments-for-github-actions#github_token-secret
+    const myToken = core.getInput('myToken');
+
+    const octokit = new github.GitHub(myToken);
+
+    const { data: pullRequest } = await octokit.pulls.get({
+        owner: 'octokit',
+        repo: 'rest.js',
+        pull_number: 123,
+        mediaType: {
+          format: 'diff'
+        }
+    });
+
+    console.log(pullRequest);
    
-  const myToken = core.getInput('myToken');
-  const octokit = new github.GitHub(myToken);
-
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  //const payload = JSON.stringify(github.context.payload, undefined, 2)
-  //console.log(`The event payload: ${payload}`);
-  console.log('evenement recu');
-  actualiserTotalColonne(octokit, github.context.payload.project_card.column_id);
-  if( github.context.payload.changes && github.context.payload.changes.column_id && github.context.payload.changes.column_id.from){
+   // Get the JSON webhook payload for the event that triggered the workflow
+   //const payload = JSON.stringify(github.context.payload, undefined, 2)
+   //console.log(`The event payload: ${payload}`);
+   actualiserTotalColonne(octokit, github.context.payload.project_card.column_id);
+   if( github.context.payload.changes && github.context.payload.changes.column_id && github.context.payload.changes.column_id.from){
     actualiserTotalColonne(octokit, github.context.payload.changes.column_id.from);
-  }
-  
-} catch (error) {
-  core.setFailed(error.message);
+   }
+   
 }
+
+run();
+
+
+
+
