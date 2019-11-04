fe81cf3d888bb4fa00479f0c9b52fe189a672385
matthieuhodin <42959826+matthieuhodin@users.noreply.github.com>
2019-11-04 12:03:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,24 @@ async function actualiserTotalColonne(octokit, columnId){
    console.log('actualiserTotalColonne:'+columnId);
    const resultCards = await octokit.projects.listCards({column_id: columnId });
   
-   console.log('resultCards :');
-   console.log(JSON.stringify(resultCards, undefined, 2));  
+   for(var iCard=0; iCard<  resultCards.data.length; iCard++){
+      var card=resultCards.data[iCard];
+      var segments= card.content_url.split('/');
+      
+      var owner = segments[segments.length-4];
+      var repo=segments[segments.length-3];
+      var issue_number = segments[segments.length-1];
+      const detailIssue = await octokit.issues.get({
+                             owner,
+                             repo,
+                             issue_number
+                           });
+      console.log('DETAIL Issue '+issue_number);
+      console.log(detailIssue);
+      
+   }
+    //     "content_url": "https://api.github.com/repos/matthieuhodin/pm-actions/issues/1"
+   
 }
 
 
